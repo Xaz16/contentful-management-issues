4b4a69a344c05eaaffb7a5810b3a32703980262f
Xaz16 <gxaz16@gmail.com>
2021-05-26 16:03:59 +0300

    fix

--- getAllPreviewKeys/main.ts
+++ getAllPreviewKeys/main.ts
@@ -1,6 +1,7 @@
 require("dotenv").config({});
 
 import { createClient } from "contentful-management";
+import { ApiKey } from "contentful-management/types";
 
 const main = async () => {
   const client = createClient({ accessToken: process.env.ACCESS_TOKEN });
@@ -14,6 +15,9 @@ const main = async () => {
   // Is not allow us to use it, but we see in runtime another interface
   // @ts-ignore
   console.log(previewApiKey.accessToken);
+
+  const rightOneInterface = previewApiKey as ApiKey;
+  console.log(rightOneInterface.accessToken);
 };
 
 main();
