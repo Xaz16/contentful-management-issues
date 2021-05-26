bd17f83ef9d45fec2d3ee1e4db6f4b251cba33fa
Aleksey Chipiga <achipiga@shakuro.com>
2021-05-26 15:43:29 +0300

    initial commit

---
+++ createUiExtensionWithId/README.md
@@ -0,0 +1,7 @@
+## Type issue demonstration
+
+Steps:
+
+- npm ci
+- create .env file from .env.example and run npm start
+- npm start
---
+++ createUiExtensionWithId/main.ts
@@ -0,0 +1,37 @@
+require("dotenv").config({});
+
+import { createClient } from "contentful-management";
+import { CreateUIExtensionProps } from "contentful-management/types";
+
+const main = async () => {
+  const client = createClient({ accessToken: process.env.ACCESS_TOKEN });
+  const space = await client.getSpace(process.env.YOUR_SPACE_ID);
+  const environment = await space.getEnvironment(process.env.YOUR_ENV_ID);
+  const extension: CreateUIExtensionProps = {
+    name: "test extension",
+    fieldTypes: [{ type: "Symbol" }],
+    sidebar: false,
+    srcdoc: null,
+  };
+  const extensionId = "anyIdAsYouWish";
+
+  const notWorkingOne = async () => {
+    await environment.createUiExtensionWithId(extensionId, extension);
+  };
+  const workingOne = async () => {
+    await environment.createUiExtensionWithId(extensionId, {
+      extension,
+    } as any);
+  };
+
+  try {
+    await notWorkingOne();
+  } catch (e) {
+    console.error("Right interface, but error");
+  }
+
+  await workingOne();
+  console.log("Success!");
+};
+
+main();
